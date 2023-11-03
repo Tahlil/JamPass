02e42550ac07713c5cd219639d9e6febd29ea0ea
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-04 04:11:24 +0600

    Add events cards

--- frontend/src/components/events/index.tsx
+++ frontend/src/components/events/index.tsx
@@ -10,32 +10,34 @@ export function Events() {
         All Events
       </h1>
     </div>
-    <div className="w-full md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2">
-      <div className="bg-white rounded-lg m-h-64 p-2 transform hover:translate-y-2 hover:shadow-xl transition duration-300">
-        <figure className="mb-2">
-          <img src="https://srv-cdn.onedio.com/store/bf2cbc886120f284ef46fd92a48f5fb58c62e6a50fbdf8fa796d057dd0ddc242.png" alt="" class="h-64 ml-auto mr-auto" />
-        </figure>
-        <div className="rounded-lg p-4 bg-purple-700 flex flex-col">
-          <div>
-            <h5 className="text-white text-2xl font-bold leading-none">
-              iPhone 11 Pro Max
-            </h5>
-            <span className="text-xs text-gray-400 leading-none">And then there was Pro.</span>
-          </div>
-          <div className="flex items-center">
-            <div className="text-lg text-white font-light">
-              $1099,00
-            </div>
-            <button className="rounded-full bg-purple-900 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
-                <line x1="12" y1="5" x2="12" y2="19"></line>
-                <line x1="5" y1="12" x2="19" y2="12"></line>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    {EventData.map(event => 
+           <div className="w-full md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2">
+           <div className="bg-white rounded-lg m-h-64 p-2 transform hover:translate-y-2 hover:shadow-xl transition duration-300">
+             <figure className="mb-2">
+               <img src={event.image} alt="" className="h-64 ml-auto mr-auto" />
+             </figure>
+             <div className="rounded-lg p-4 bg-purple-700 flex flex-col">
+               <div>
+                 <h5 className="text-white text-2xl font-bold leading-none h-24">
+                   {event.name}
+                 </h5>
+                 <h5 className="text-xs text-gray-400 h-[150px] pt-3">{event.description}</h5>
+               </div>
+               <div className="flex items-center">
+                 <div className="text-lg text-white font-light">
+                 {event.price}
+                 </div>
+                 <button className="rounded-full bg-purple-900 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="stroke-current m-auto">
+                     <line x1="12" y1="5" x2="12" y2="19"></line>
+                     <line x1="5" y1="12" x2="19" y2="12"></line>
+                   </svg>
+                 </button>
+               </div>
+             </div>
+           </div>
+         </div>
+        )}
   </div>
 </div>
 
