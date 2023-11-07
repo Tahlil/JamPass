fae6416b91ef1c7e76687417490147163863d53e
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-07 14:28:19 +0600

    Add event model

---
+++ frontend/src/utils/model/event.ts
@@ -0,0 +1,15 @@
+export type Event = {
+    name: String,
+    datetime: String,
+    location: String,
+    description: String,
+    agenda: String,
+    speakers: String,
+    breaks: String,
+    registration: String,
+    contact: String,
+    emergency: String,
+    image: String,
+    price: String,
+    transferrable: Boolean
+}
