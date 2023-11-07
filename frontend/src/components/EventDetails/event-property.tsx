cce8533ab5b04cd6d593b5794f3cd4ad7a6dca8a
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-07 11:48:11 +0600

    Inject value to event property

--- frontend/src/components/EventDetails/event-property.tsx
+++ frontend/src/components/EventDetails/event-property.tsx
@@ -1,10 +1,11 @@
 export function EventProperty(props: any) {
+    console.log({props})
     return (
         <div className="grid grid-cols-12 place-items-start">
          <div className="col-span-2 text-left">Name:</div>
           <div className="col-span-10 mb-3">
             <p className="nes-container is-rounded bg-gradient-to-r from-cyan-500 to-blue-500">
-              Event Name
+              {props.name}
             </p>
           </div>
           </div>
--- frontend/src/components/EventDetails/index.tsx
+++ frontend/src/components/EventDetails/index.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import EventProperty from "./event-property"
 export function EventDetails(props: any) {
+  props = {name: "Test Event"}
   return (
     <div className="nes-container with-title">
       <p className="title">Event Details</p>
-      <EventProperty/>
+      <EventProperty name= "Test Event"/>
 
     </div>
   );
