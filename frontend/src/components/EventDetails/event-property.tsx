56e8aec10cb025185158014d969fdc50a25706a4
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-07 14:54:29 +0600

    Add event details page

--- frontend/src/components/EventDetails/event-property.tsx
+++ frontend/src/components/EventDetails/event-property.tsx
@@ -2,10 +2,10 @@ export function EventProperty(props: any) {
     console.log({props})
     return (
         <div className="grid grid-cols-12 place-items-start">
-         <div className="col-span-2 text-left">Name:</div>
-          <div className="col-span-10 mb-3">
+         <div className="col-span-3 text-left uppercase">{props.keyName}:</div>
+          <div className="col-span-9 mb-3">
             <p className="nes-container is-rounded bg-gradient-to-r from-cyan-500 to-blue-500">
-              {props.name}
+              {props.name.toString()}
             </p>
           </div>
           </div>
--- frontend/src/components/EventDetails/index.tsx
+++ frontend/src/components/EventDetails/index.tsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
 import EventProperty from "./event-property"
-export function EventDetails(eventProps: any) {
-  console.log({eventProps})
+import { Event } from "@/utils/model/event"
+
+
+export function EventDetails(eventProps: Event) {
+ 
+  const eventDetails:any = Object.assign({}, eventProps);
   return (
     <div className="nes-container with-title">
       <p className="title">Event Details</p>
-      <EventProperty key="" name= "Test Event"/>
+      <div>
+        {
+            Object.keys(eventProps).map(key => (
+                <EventProperty keyName={key} name={eventDetails[key]}/>
+            ))
+        }
+      </div>
+      {/* {
+        Object.keys(eventProps).map((e) => {
+            <EventProperty key={e} name={e}/>
+        })
+      } */}
 
     </div>
+   
   );
 }
